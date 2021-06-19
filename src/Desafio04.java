1071c7dd91558f5b2f36560d4589c3224e865bcd
Andre David <prof.afdavid@gmail.com>
2021-06-19 12:59:27 -0300

    Alteração nos enunciados

--- src/Desafio04.java
+++ src/Desafio04.java
@@ -1,10 +1,9 @@
 
 public class Desafio04 {
-	/*Você deve se lembrar que os triângulos possuem uma regra para existirem: a soma de QUAISQUER dois lados tem que ser maior do que o lado restante.
-E deve se lembrar, também, que os triângulos podem ser de três tipos: equiláteros, escalenos ou isósceles.
-Crie uma função que valida se um triângulo existe ou não. Além disso, crie uma função que verifica o tipo do triângulo (ela deve primeiro validar se o triângulo existe).
-A main do seu programa deve ter a leitura dos lados do triângulo e a chamada da função que retorna o tipo dele. 
-
+	/* 
+	Considere o cálculo do salário de um professor, disponível em http://www1.sinprosp.org.br/guia_consultas.asp?mat=8
+	
+	Crie as funções que julgar necessárias para obter cada um dos valores que fazem parte do salário de um professor
 	 * */
 	public static void main(String[] args) {
 		
--- src/Desafio05.java
+++ src/Desafio05.java
@@ -1,6 +1,6 @@
 
 public class Desafio05 {
-/*A rede de supermercados Carre4 está levando a sério as restrições de isolamento social no Brasil. Estão trabalhando para automatizar as portas das unidades, permitindo a entrada apenas das pessoas que estão atendendo às solicitações.
+/*A rede de supermercados MercaJava está levando a sério as restrições de isolamento social no Brasil. Estão trabalhando para automatizar as portas das unidades, permitindo a entrada apenas das pessoas que estão atendendo às solicitações.
 Toda a identificação das medidas necessárias é feita através de câmeras e sensores de temperaturas. Sua tarefa é criar as funções que recebam essas medidas e informem se o cliente pode ou não entrar no estabelecimento.
 As funções são:
 •	Temperatura: Clientes com temperatura superior a 37,0 graus não podem entrar no estabelecimento.
